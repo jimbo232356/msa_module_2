9f2e04e903c04932a7e18279b5eadffa55e96662
Seungmin Lee <Seungmin Lee>
2016-09-02 22:32:17 +1200

    testing purpose

--- js/main.ts
+++ js/main.ts
@@ -1,19 +1,19 @@
 // Get elements from DOM
 var pageheader = $("#page-header")[0];
 var pagecontainer = $("#page-container")[0];
-var inputsearch = $("#search")[0];
+var search = $("#search")[0];
 var searchbutton = $("#SearchButton")[0];
-var result = $("resultsDiv")[0];
+var resultsdiv = $("resultsDiv")[0];
 
 // Register button listeners
 searchbutton.addEventListener("click", function () {
     pageheader.innerHTML = "Just a sec while we find";
-    result.innerHTML = "Hello1";
+    resultsdiv.innerHTML = "Hello1";
     changeUI();
 });
 
 function changeUI() : void {
     pageheader.innerHTML = "Here is a result";
-    result.innerHTML = "Hello-2";
+    resultsdiv.innerHTML = "Hello-2";
     searchbutton.style.display = "none";
 }
