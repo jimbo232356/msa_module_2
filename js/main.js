ca63a92729308174a52a8f2267ca5d43542ed8da
Seungmin Lee <Seungmin Lee>
2016-09-02 17:47:44 +1200

    test2

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Get elements from DOM
+var pageheader = $("#page-header")[0];
+var pagecontainer = $("#page-container")[0];
+var inputsearch = $("#InputSearch")[0];
+var refreshbtn = $("#refreshbtn")[0];
+// Register button listeners
+refreshbtn.addEventListener("click", function () {
+    pageheader.innerHTML = "Just a sec while we find";
+});
