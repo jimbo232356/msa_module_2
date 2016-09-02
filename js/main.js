0f3c087b19691cf2681eb1dfe0ce113884eaf659
Seungmin Lee <Seungmin Lee>
2016-09-02 20:03:15 +1200

    testing result pop

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ var pageheader = $("#page-header")[0];
 var pagecontainer = $("#page-container")[0];
 var inputsearch = $("#InputSearch")[0];
 var searchbutton = $("#SearchButton")[0];
+var result = $("resultsDiv")[0];
 // Register button listeners
 searchbutton.addEventListener("click", function () {
     pageheader.innerHTML = "Just a sec while we find";
-    changeUI();
+    changeUI(inputsearch);
 });
-function changeUI() {
+function changeUI(r) {
     pageheader.innerHTML = "Here is a result";
+    result.innerHTML = r;
 }
--- js/main.ts
+++ js/main.ts
@@ -1,15 +1,17 @@
 // Get elements from DOM
 var pageheader = $("#page-header")[0];
 var pagecontainer = $("#page-container")[0];
-var inputsearch= $("#InputSearch")[0];
-var searchbutton= $("#SearchButton")[0];
+var inputsearch = $("#InputSearch")[0];
+var searchbutton = $("#SearchButton")[0];
+var result = $("resultsDiv")[0];
 
 // Register button listeners
 searchbutton.addEventListener("click", function () {
     pageheader.innerHTML = "Just a sec while we find";
-    changeUI();
+    changeUI(inputsearch);
 });
 
-function changeUI() : void {
+function changeUI(r) : void {
     pageheader.innerHTML = "Here is a result"
+    result.innerHTML = r;
 }
