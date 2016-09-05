510cabae3bafb3ce3b9e4f5435dc668354b6bb5d
Seungmin Lee <Seungmin Lee>
2016-09-05 15:35:13 +1200

    05092016 develop

--- js/main.js
+++ js/main.js
@@ -68,7 +68,7 @@ function callBingRequest(s, callback) {
         data: "{body}",
     })
         .done(function (data) {
-        if (data.webPages.value != {}) {
+        if (data.rankingRespons != {}) {
             // Get the results
             var webpages = data.webPages.value;
             callback(webpages);
