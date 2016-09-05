9918744226d4e93bd48258ee8984cddc3394e5db
Seungmin Lee <Seungmin Lee>
2016-09-05 15:21:38 +1200

    05092016 develop

--- js/main.ts
+++ js/main.ts
@@ -74,7 +74,7 @@ function callBingRequest(s, callback) : void {
         data: "{body}",
     })
         .done(function (data) {
-            if (data.length != 0) { // if a results are detected
+            if (data.webPages.length != 0) { // if a results are detected
                     // Get the results
                     var webpages = data.webPages.value;
                     callback(webpages);
