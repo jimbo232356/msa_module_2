8430006903c70c42df11d4c6a3f8a0e91da0ce15
Seungmin Lee <Seungmin Lee>
2016-09-05 13:55:32 +1200

    05092016 develop

--- js/main.ts
+++ js/main.ts
@@ -49,7 +49,6 @@ function loadResults(div, searchResults) : void {
 function resultshape(searchDisplay) {
 	var arr = [
 		"<div class='webResult'>",
-        "<body link='red'>",
 		"<h2><a href=" + searchDisplay.url + ">" + searchDisplay.name + "</a></h2>",
 		"<p>" + searchDisplay.snippet + "</p>",
 		"<a href=" + searchDisplay.url + ">" + searchDisplay.displayUrl + "</a>",
