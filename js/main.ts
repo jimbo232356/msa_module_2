8d75db985919cda89c8ae6d53952859e0f4e0a79
jimbo232356 <jimbo232356@gmail.com>
2022-02-07 21:16:49 +0900

    Update main.ts

--- js/main.ts
+++ js/main.ts
@@ -9,7 +9,7 @@ var searchCount = 5;
 
 // Register button listeners
 searchbutton.addEventListener("click", function () {
-    pageheader.innerHTML = "Just a sec while we find";
+    pageheader.innerHTML = "Processing...";
     var div = resultsdiv;
     var s = (<HTMLInputElement>document.getElementById("search")).value;
     callBingRequest(s, function(searchResults) {
@@ -80,12 +80,12 @@ function callBingRequest(s, callback) : void {
                     var webpages = data.webPages.value;
                     callback(webpages);
             } else {
-                pageheader.innerHTML = "Hmm, we can't find any results. Try another?";
+                pageheader.innerHTML = "No Results \n Please enter other words";
                 $("#resultsDiv").empty();
             }
         })
         .fail(function (error) {
-            pageheader.innerHTML = "Sorry, something went wrong. :( Try again in a bit?";
+            pageheader.innerHTML = "Out of Orders :(";
             console.log(error.getAllResponseHeaders());
         });
 }
